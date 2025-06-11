a312caffd5d22b6fd6635c13b6425d1764e67d56
Stanislav Severin <severin1975list@gmail.com>
2025-06-11 14:51:23 +0300

    Additional changes

--- Abstractions/InMemoryUserSessionStore.cs
+++ Abstractions/InMemoryUserSessionStore.cs
@@ -1,16 +1,15 @@
-﻿namespace CarInsuranceTgBot.Abstractions;
+﻿using System.Collections.Concurrent;
+
+namespace CarInsuranceTgBot.Abstractions;
 
 // Імітація бд, де long - це ID користувача, а UserSession - це допоміжні дані
 public class InMemoryUserSessionStore : IUserSessionStore
 {
-    private readonly Dictionary<long, UserSession> _sessions = new();
+    // зберігаю стан користувачів у багатопотоковому середовищі
+    private readonly ConcurrentDictionary<long, UserSession> _sessions = new();
 
     public UserSession GetOrCreateSession(long userId)
     {
-        // Імітація наче в базу був доданий користувач, або отриманий інсуючий
-        if (!_sessions.ContainsKey(userId))
-            _sessions[userId] = new UserSession();
-
-        return _sessions[userId];
+        return _sessions.GetOrAdd(userId, _ => new UserSession());
     }
 }
--- Services/MindeeService.cs
+++ Services/MindeeService.cs
@@ -4,6 +4,7 @@ using Mindee;
 using Telegram.Bot;
 using Mindee.Http;
 using Mindee.Product.Generated;
+using Mindee.Parsing.Generated;
 
 namespace CarInsuranceTgBot.Services;
 
@@ -13,7 +14,7 @@ public class MindeeService : IMindeeService
 
     public MindeeService(IConfiguration configuration)
     {
-        _apiKey = configuration["MindeeApi:CarInsuranceBotKey"] ?? throw new InvalidOperationException("Mindee API key не знайдено в конфігурації."); ;
+        _apiKey = configuration["MindeeApi:CarInsuranceBotKey"] ?? throw new InvalidOperationException("Mindee API key не знайдено в конфігурації.");
     }
 
     public async Task<string> ExtractPassportDataAsync(string fileId, ITelegramBotClient botClient, CancellationToken ct)
@@ -45,11 +46,11 @@ public class MindeeService : IMindeeService
 
         var prediction = response.Document.Inference.Prediction;
 
-        var documentNumber = prediction.Fields["document_number"].AsStringField().Value;
-        var middleName = prediction.Fields["middle_name"].AsStringField().Value;
-        var name = prediction.Fields["name"].AsStringField().Value;
-        var surname = prediction.Fields["surname"].AsStringField().Value;
-        var validUntil = prediction.Fields["valid_until"].AsDateField().Value.ToString();
+        var documentNumber = SafeGetField(prediction.Fields, "document_number");
+        var middleName = SafeGetField(prediction.Fields, "middle_name");
+        var name = SafeGetField(prediction.Fields, "name");
+        var surname = SafeGetField(prediction.Fields, "surname");
+        var validUntil = SafeGetField(prediction.Fields, "valid_until");
 
         var formattedText = $"Документ №: {documentNumber} \n" +
                             $"Ім'я: {name} \n" +
@@ -89,14 +90,13 @@ public class MindeeService : IMindeeService
 
         var prediction = response.Document.Inference.Prediction;
 
-        var registration_number = prediction.Fields["registration_number"].AsStringField().Value;
-        var date_of_first_registration = prediction.Fields["date_of_first_registration"].AsDateField().Value.ToString();
-        var date_of_registration = prediction.Fields["date_of_registration"].AsDateField().Value.ToString();
-        var year_of_manufacture = prediction.Fields["year_of_manufacture"].AsDateField().Value.ToString();
-        var surname_or_company = prediction.Fields["surname_or_company"].AsStringField().Value;
-        var given_name = prediction.Fields["given_name"].AsStringField().Value;
-        var adressa = prediction.Fields["adressa"].AsStringField().Value;
-
+        var registration_number = SafeGetField(prediction.Fields, "registration_number");
+        var date_of_first_registration = SafeGetField(prediction.Fields, "date_of_first_registration");
+        var date_of_registration = SafeGetField(prediction.Fields, "date_of_registration");
+        var year_of_manufacture = SafeGetField(prediction.Fields, "year_of_manufacture");
+        var surname_or_company = SafeGetField(prediction.Fields, "surname_or_company");
+        var given_name = SafeGetField(prediction.Fields, "given_name");
+        var adressa = SafeGetField(prediction.Fields, "adressa");
 
         var formattedText = $"Реєстраційний номер №: {registration_number} \n" +
                             $"Дата першої реєстрації: {date_of_first_registration} \n" +
@@ -108,5 +108,10 @@ public class MindeeService : IMindeeService
 
 
         return formattedText;
+    }
+
+    private static string SafeGetField(Dictionary<string, GeneratedFeature> fields, string fieldName)
+    {
+        return fields.TryGetValue(fieldName, out var field) && field?.AsStringField().Value is string value ? value : "";
     }
 }
